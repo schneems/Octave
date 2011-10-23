b8a27c27434fe2804a270046b4082fab081504af
schneems <richard.schneeman@gmail.com>
2011-10-23 17:58:40 -0500

    part 3 success

--- mlclass-ex1/gradientDescent.m
+++ mlclass-ex1/gradientDescent.m
@@ -1,6 +1,6 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Performs gradient descent to learn theta
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
@@ -11,23 +11,26 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
 
 
+    x = X(:,2);
+    h = theta(1) + (theta(2)*x);
 
+    theta_zero = theta(1) - alpha * (1/m) * sum(h-y);
+    theta_one  = theta(2) - alpha * (1/m) * sum((h - y) .* x);
 
-
-
-
+    theta = [theta_zero; theta_one];
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCost(X, y, theta);
-
+    % disp(J_history(iter));
 end
 
+  disp(min(J_history));
 end
--- mlclass-ex1/plotData.m
+++ mlclass-ex1/plotData.m
@@ -14,6 +14,10 @@ function plotData(x, y)
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
+ylabel('Profit in $10,000s'); % Set the y−axis label
+xlabel('Population of City in 10,000s'); % Set the x−axis label
+
 figure; % open a new figure window
 
 
