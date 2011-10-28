297497359058493cc296691ffc1e87929cfef0fc
schneems <richard.schneeman@gmail.com>
2011-10-28 11:52:25 -0500

    week 2 exercise 1

--- mlclass-ex2/mlclass-ex2/sigmoid.m
+++ mlclass-ex2/mlclass-ex2/sigmoid.m
@@ -3,14 +3,13 @@ function g = sigmoid(z)
 %   J = SIGMOID(z) computes the sigmoid of z.
 
 % You need to return the following variables correctly 
-g = zeros(size(z));
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
 
-
+g = 1 ./ (1 + e.^-z)
 
 
 % =============================================================
