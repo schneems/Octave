75b907af2d30b06655643a8cd8b2a8e3067211ea
schneems <richard.schneeman@gmail.com>
2011-10-29 17:38:07 -0500

    week 2 questions 2 & 3

--- mlclass-ex2/mlclass-ex2/costFunction.m
+++ mlclass-ex2/mlclass-ex2/costFunction.m
@@ -20,12 +20,10 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-
-
-
-
-
-
+h = sigmoid(X*theta);
+% J = (1/m)*sum(-y .* log(h) - (1 - y) .* log(1-h));
+J = (1/m)*(-y'* log(h) - (1 - y)'* log(1-h));
+grad = (1/m)*X'*(h - y);
 
 % =============================================================
 
