b93bcb572685419daaf80bd402430e07baf0b63b
schneems <richard.schneeman@gmail.com>
2011-10-30 15:16:06 -0500

    end of week 2

--- mlclass-ex2/mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/mlclass-ex2/costFunctionReg.m
@@ -18,9 +18,18 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+h = sigmoid(X*theta);
+% J = (1/m)*sum(-y .* log(h) - (1 - y) .* log(1-h));
+shift_theta = theta(2:size(theta));
+theta_reg = [0;shift_theta];
 
+J = (1/m)*(-y'* log(h) - (1 - y)'*log(1-h))+(lambda/(2*m))*theta_reg'*theta_reg;
 
+% grad_zero = (1/m)*X(:,1)'*(h-y);
+% grad_rest = (1/m)*(shift_x'*(h - y)+lambda*shift_theta);
+% grad      = cat(1, grad_zero, grad_rest);
 
+grad = (1/m)*(X'*(h-y)+lambda*theta_reg);
 
 % =============================================================
 
