5b92130bbe67f934ea58bc619f81f8651be01a22
schneems <richard.schneeman@gmail.com>
2011-11-06 15:20:44 -0600

    question 1

--- mlclass-ex3/mlclass-ex3/lrCostFunction.m
+++ mlclass-ex3/mlclass-ex3/lrCostFunction.m
@@ -37,16 +37,20 @@ grad = zeros(size(theta));
 %
 
 
+% calculate hypothesis
+h = sigmoid(X*theta);
 
+% regularize theta by removing first value
+theta_reg = [0;theta(2:end, :);];
 
+J = (1/m)*(-y'* log(h) - (1 - y)'*log(1-h))+(lambda/(2*m))*theta_reg'*theta_reg;
 
+grad = (1/m)*(X'*(h-y)+lambda*theta_reg);
 
 
 
 
 
 % =============================================================
-
-grad = grad(:);
 
 end
