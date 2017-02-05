04570984432c95543d52983de8db699abbdb0a28
ankursi <ankursi@iitk.ac.in>
2017-02-06 02:15:06 +0530

    Completing some files of mlclass-ex1 and mlclass-ex2

--- mlclass-ex1/featureNormalize.m
+++ mlclass-ex1/featureNormalize.m
@@ -15,7 +15,7 @@ sigma = zeros(1, size(X, 2));
 %               of the feature and subtract it from the dataset,
 %               storing the mean value in mu. Next, compute the 
 %               standard deviation of each feature and divide
-%               each feature by it's standard deviation, storing
+%               each feature by its standard deviation, storing
 %               the standard deviation in sigma. 
 %
 %               Note that X is a matrix where each column is a 
@@ -26,15 +26,9 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-mu    = mean(X)
-sigma = std(X)
-
-indicies = 1:size(X, 2);
-
-for i = indicies,
-  XminusMu  = X(:, i) - mu(i);
-  X_norm(:, i) = XminusMu / sigma(i);
-end
+mu = mean(X);
+sigma = std(X);
+X_norm = (X .- mu)./sigma
 
 
 % ============================================================
