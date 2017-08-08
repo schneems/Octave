5d7c7cfb79a13897bff79d9a9f7ceb058921c0e8
JACK_1729 <jack17529@users.noreply.github.com>
2017-08-08 12:36:22 +0530

    Update plotData.m
    'x' is changed to 'X'

--- mlclass-ex1/plotData.m
+++ mlclass-ex1/plotData.m
@@ -14,7 +14,7 @@ function plotData(x, y)
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
-plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
+plot(X, y, 'rx', 'MarkerSize', 10); % Plot the data
 ylabel('Profit in $10,000s'); % Set the y−axis label
 xlabel('Population of City in 10,000s'); % Set the x−axis label
 
