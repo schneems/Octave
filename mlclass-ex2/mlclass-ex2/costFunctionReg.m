4d24d084ee6c70582658592acb9e62e4313a5192
Djotchuang <36383624+Djotchuang@users.noreply.github.com>
2019-10-05 11:12:04 +0100

    Update to the Gradient
    You obmitted the "/m" for lambda when evaluating the gradient.

--- mlclass-ex2/mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/mlclass-ex2/costFunctionReg.m
@@ -23,13 +23,13 @@ h = sigmoid(X*theta);
 shift_theta = theta(2:size(theta));
 theta_reg = [0;shift_theta];
 
-J = (1/m)*(-y'* log(h) - (1 - y)'*log(1-h))+(lambda/(2*m))*theta_reg'*theta_reg;
+J = (1/m)*(-y'* log(h) - (1 - y)'*log(1-h)) + (lambda/(2*m))*theta_reg'*theta_reg;
 
 % grad_zero = (1/m)*X(:,1)'*(h-y);
 % grad_rest = (1/m)*(shift_x'*(h - y)+lambda*shift_theta);
 % grad      = cat(1, grad_zero, grad_rest);
 
-grad = (1/m)*(X'*(h-y)+lambda*theta_reg);
+grad = (1/m)*(X'*(h-y) + (lambda/m)*theta_reg);
 
 % =============================================================
 
